3f428aa226971ef7e20270c9c67cc3e2ff40614d
joaomiranda15 <paulocjpaulo@gmail.com>
2026-08-07 17:59:49 -0300

    feat: atualizar widget de calculadora de treinamento

--- Widget/wcm/widget/calculadora_treinamento/src/main/webapp/resources/js/calculadora_treinamento.js
+++ Widget/wcm/widget/calculadora_treinamento/src/main/webapp/resources/js/calculadora_treinamento.js
@@ -1,4 +1,3 @@
-"use strict"
 var MyWidget = SuperWidget.extend({
     //variáveis da widget
     variavelNumerica: null,
@@ -7,13 +6,13 @@ var MyWidget = SuperWidget.extend({
 
     //método iniciado quando a widget é carregada
     init: function () {
-        this.criarTemplate();
-        this.criarObjeto();
-        this.spreadOperator();
-        this.restOperator();
-        this.gerarObjeto("aleatorio");
-        // this.metodoAleatorio();
-        this.jQuery();
+        // this.criarTemplate();
+        // this.criarObjeto();
+        // this.spreadOperator();
+        // this.restOperator();
+        // this.gerarObjeto("aleatorio");
+        // // this.metodoAleatorio();
+        // this.jQuery();
     },
 
     //BIND de eventos
@@ -45,92 +44,108 @@ var MyWidget = SuperWidget.extend({
     },
 
     confirmar: function (htmlElement, event) {
-        let v1 = Number($("#valor1").val());
-        let v2 = Number($("#valor2").val());
+        let idBase = this.instanceId; // Criando a variável para o instanceId dos elementos na Widget
+        let valor1Id = '#valor1_' + idBase; // Concatenando o id + o instanceId
+        let valor2Id = '#valor2_' + idBase;
+        let resultado = '#resultado_' + idBase;
 
-        if (this.operacaoAtual === 'soma') {
-            $("#resultado").val(v1 + v2);
+        this.valorVazio();
+
+        let v1 = Number($(valor1Id).val());
+        let v2 = Number($(valor2Id).val());
+
+        if (!this.operacaoAtual) {
+            this.exibirAvisoToast('Selecione uma operação entes de enviar.');
+            return;
+        }
+        else if (this.operacaoAtual === 'soma') {
+            $(resultado).val(v1 + v2);
         } else if (this.operacaoAtual === 'subtracao') {
-            $("#resultado").val(v1 - v2);
+            $(resultado).val(v1 - v2);
         } else if (this.operacaoAtual === 'multiplicacao') {
-            $("#resultado").val(v1 * v2);
+            $(resultado).val(v1 * v2);
         } else if (this.operacaoAtual === 'divisao') {
             if (v2 === 0) {
                 this.exibirAvisoToast('Divisão por 0 não é permitida!');
             } else {
-                $("#resultado").val(v1 / v2);
+                $(resultado).val(v1 / v2);
             }
         }
     },
 
     exibirAvisoToast: function (message) {
-            
         FLUIGC.toast({
             title: 'Atenção:',
-            message: 'A operação selecionada foi a divisão por 0. Por favor troque o número antes de concluir a operação.',
+            message: message,
             type: 'warning'
         });
     },
 
-    valorVazio: function (message) {
-        if ($("#valor1").val() == '') {
-            $("#valor1").val(0);
+    valorVazio: function () {
+        let idBase = this.instanceId; // Criando a variável para o instanceId dos elementos na Widget
+        let valor1Id = '#valor1_' + idBase; // Concatenando o id + o instanceId
+        let valor2Id = '#valor2_' + idBase;
 
-        } else if ($("#valor2") == '') {
-            $("#valor2").val(0)
+        if ($(valor1Id).val() === '') {
+            $(valor1Id).val(0);
+
+        } 
+
+        if ($(valor2Id) === '') {
+            $(valor2Id).val(0);
         }
-    },
+    }
 
-    criarTemplate: function() {
-        const numeros = [10, , 30];
+    // criarTemplate: function() {
+    //     const numeros = [10, , 30];
 
-        const [a, b = 20, c] = numeros; // Quando o b for vazio, segundo a chamada de números, então, o valor dele será 20.
+    //     const [a, b = 20, c] = numeros; // Quando o b for vazio, segundo a chamada de números, então, o valor dele será 20.
 
-        console.log(a, b, c);
-    },
-
-    criarObjeto: function() {
-        const usuario = { nome: "Bruno", idade: 30};
-        const { nome: n, idade: i } = usuario;
-        console.log(n,i);
-    },
-
-    spreadOperator: function() {
-        const numeros = [1,2,3];
-        const novosNumeros = [...numeros,4,5,6,7]; // A nova Array vai receber os valores da primeira Array, com os novos números que foram adicionados ("4,...,7")
-
-        console.log(novosNumeros);
-    },
-
-    restOperator() {
-        function soma(...numeros) {
-            return numeros.reduce((total, n) => total + n, 0)
-        }
-        console.log(soma(1,2,3,4));
-    },
-
-    gerarObjeto(aleatorio) {
-        const campo = "email";
-        const usuario = {
-            nome: "Brunalha",
-            [campo]: "brunera@email.com",
-            [aleatorio]: "Aleatório"
-        };
-        console.log(campo, usuario)
-    },
-
-    // metodoAleatorio() {
-    //     valor = 10;
-    //     console.log(valor)
+    //     console.log(a, b, c);
     // },
 
-    jQuery() {
-        $('.mensagem').css('color', 'blue');
+    // criarObjeto: function() {
+    //     const usuario = { nome: "Bruno", idade: 30};
+    //     const { nome: n, idade: i } = usuario;
+    //     console.log(n,i);
+    // },
 
-        // Usando js moderno
+    // spreadOperator: function() {
+    //     const numeros = [1,2,3];
+    //     const novosNumeros = [...numeros,4,5,6,7]; // A nova Array vai receber os valores da primeira Array, com os novos números que foram adicionados ("4,...,7")
 
-        this.DOM.querySelector('.mensagem').style.backgroundColor = 'green'; 
-        // DOM serve para mostrar para o js que só se quer realizar a alteração na Widget.
-    }
+    //     console.log(novosNumeros);
+    // },
+
+    // restOperator() {
+    //     function soma(...numeros) {
+    //         return numeros.reduce((total, n) => total + n, 0)
+    //     }
+    //     console.log(soma(1,2,3,4));
+    // },
+
+    // gerarObjeto(aleatorio) {
+    //     const campo = "email";
+    //     const usuario = {
+    //         nome: "Brunalha",
+    //         [campo]: "brunera@email.com",
+    //         [aleatorio]: "Aleatório"
+    //     };
+    //     console.log(campo, usuario)
+    // },
+
+    // // metodoAleatorio() {
+    // //     valor = 10;
+    // //     console.log(valor)
+    // // },
+
+    // jQuery() {
+    //     $('.mensagem').css('color', 'blue');
+
+    //     // Usando js moderno
+
+    //     this.DOM.querySelector('.mensagem').style.backgroundColor = 'green'; 
+    //     // DOM serve para mostrar para o js que só se quer realizar a alteração na Widget.
+    // }
 });
 
